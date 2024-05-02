f71e712df56779bc499bc3633b4450c16f09f8f8
Hmiddouch Abdessadek <abdessadekhm@gmail.com>
2024-05-02 12:11:42 +0100

    Add Home page

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php include('includes/header.php'); ?>
-
-
+<?php include('includes/navbar.php');?>
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h4>Login and Registration System in PHP</h4>
+                <h5>With Email Verfication</h5>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php include('includes/footer.php'); ?>
