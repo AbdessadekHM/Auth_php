ae2ae569544046690feec443bc192193fbf8deff
Hmiddouch Abdessadek <abdessadekhm@gmail.com>
2024-05-02 13:09:26 +0100

    Fix title & scrollbar bugs

--- includes/header.php
+++ includes/header.php
@@ -4,8 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login and Registration System in PHP with Email Verfication</title>
+    <title>
+        <?php if($page_title) echo "$page_title" ?>
+    </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 
-<body>
+<body style="overflow : hidden">
