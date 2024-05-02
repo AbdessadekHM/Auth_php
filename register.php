f2124a463d3f59e022e1e671576960ea8bb0ec6e
Hmiddouch Abdessadek <abdessadekhm@gmail.com>
2024-05-02 13:10:50 +0100

    Add Register Page

---
+++ register.php
@@ -0,0 +1,52 @@
+<?php 
+$page_title = "Register Page";
+$page_loc = "Register"
+?>
+<?php include('includes/header.php'); ?>
+<?php include('includes/navbar.php');?>
+
+<div class="py-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card shadow">
+                    <div class="card-header">
+                        <h5>Registration Form</h5>
+                    </div>
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="form-group mb-3">
+                                <label for="">Name</label>
+                                <input type="text" name="name" id="" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Phone Number</label>
+                                <input type="text" name="phone" id="" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Email Address</label>
+                                <input type="email" name="email" id="" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Password</label>
+                                <input type="password" name="password" id="" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Confirm Password</label>
+                                <input type="password" name="confirm_password" id="" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <button class="btn btn-primary" type="submit">
+                                    Register
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include('includes/footer.php') ?>
